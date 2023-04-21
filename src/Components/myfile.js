2c47d147daf828fab1091e1ca37885ec8d2b4509
pragati-sriv <ps028ideal@gmail.com>
2023-04-21 15:09:58 +0530

    done

---
+++ src/Components/myfile.js
@@ -0,0 +1,5 @@
+{/* <p>
+        I am having <Text txt={subject} /> next Friday at my Home . Would you
+        like to come ? It will be fun . Lots of people from my school are <br />
+        coming . you know some of them <Text txt={friends} />
+      </p> */}
